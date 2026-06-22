a03d1b40e2ecc6d99ebc216d421f9a4674d9a0a3
Promise Okere <Promise.Okere@interswitchng.com>
2026-06-22 12:50:00 +0100

    Registration Integratin

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,12 +1,16 @@
 const Joi = require("joi");
+const AuthModel = require("../models/auth.model");
+
 // AUTH CONTROLLERS
 const register = async (req, res, next) => {
+  // Request data schema
   const authValidate = Joi.object({
     name: Joi.string().min(3).max(20).required().trim(),
     email: Joi.string().email().required().trim(),
     password: Joi.string().required(),
   });
 
+  // Validate request data
   const { error, value } = authValidate.validate(req.body);
 
   if (error) {
@@ -16,15 +20,23 @@ const register = async (req, res, next) => {
     });
   }
 
-  console.log("Joi Validation :", value);
+  const { name, email, password } = req.body;
+
+  // Check for duplicate user's
+  const validateEmail = await AuthModel.findOne({ email: email });
+
+  console.log("Validate Email :", validateEmail);
+
+  if (validateEmail) {
+    return res.status(400).json({ message: "User already exits" });
+  }
+
+  const newUser = new AuthModel(value);
+  await newUser.save();
 
   return res.status(200).json({
     message: "Register data received",
-    data: {
-      name: value.name,
-      email: value.email,
-      password: value.email,
-    },
+    data: value,
   });
 };
 
